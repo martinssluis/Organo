c9e08d8bc91c23d74dbb394fecd91772e2e82990
Luis <martinsluisdev@gmail.com>
2026-07-16 07:38:42 -0300

    feat(app.ts): adiciona componente Livro ao app

--- organo/src/app/app.ts
+++ organo/src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { Cabecalho } from './componentes/cabecalho/cabecalho';
 import { Rodape } from './componentes/rodape/rodape';
+import { Livro } from './componentes/livro/livro';
 
 @Component({
   selector: 'app-root',
   imports: [
     Cabecalho,
-    Rodape
+    Rodape,
+    Livro
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
