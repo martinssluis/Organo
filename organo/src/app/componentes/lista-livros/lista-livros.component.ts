2bac8050148655cc2c2bd5e9126b2f6cbe3cdfb4
Luis <martinsluisdev@gmail.com>
2026-07-17 08:15:55 -0300

    feat(lista-livros): cria component de lista de livros

---
+++ organo/src/app/componentes/lista-livros/lista-livros.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-livros',
+  imports: [],
+  templateUrl: './lista-livros.component.html',
+  styleUrl: './lista-livros.component.css',
+})
+export class ListaLivrosComponent {}
